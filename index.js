ed8c4782589b1cee387d5b5eaa3fa744232b31c8
= <=>
2017-11-21 19:58:35 -0200

    Configurando webdriver

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 //java -jar -Dwebdriver.chrome.driver=./chromedriver.exe selenium-server-standalone-3.7.1.jar
 
 const {Builder, By, Key, until} = require('selenium-webdriver');
+const wdConfig = require('./wdio.conf');
+const userConfig = require("./user.conf");
 
+var webdriverio = require('webdriverio');
+var browser = webdriverio.remote(config);
 
 function sleep(x) {
     return function(cb) {
@@ -10,11 +14,30 @@ function sleep(x) {
     }
  }
 
-let driver = new Builder().forBrowser('chrome').build();
+/*
+browser.init();
 
-driver.get('https://www.instagram.com');
+browser.getUrl('https://www.instagram.com');
 
 sleep(5000);
 
+browser.getText('Faça login').click();
+browser.end();
+console.log(browser.getTitle());
 
-driver.get('https://www.google.com.br');
+*/
+
+
+let driver = new Builder().forBrowser('chrome').build();
+
+driver.get('https://www.instagram.com');
+sleep(10000);
+driver.findElement(By.linkText('Faça login')).click();
+driver.findElement(By.name("username")).sendKeys(userConfig.username);
+driver.findElement(By.name("username")).sendKeys(userConfig.password);
+driver.findElement(By.()).sendKeys(userConfig.password);
+
+
+
+
+
---
+++ wdio.conf.js
@@ -0,0 +1,7 @@
+module.exports =  {
+    browserName: 'chrome',    // options: `firefox`, `chrome`, `opera`, `safari`
+    version: '27.0',          // browser version
+    platform: 'XP',           // OS platform
+    tags: ['tag1','tag2'],    // specify some tags (e.g. if you use Sauce Labs)
+    name: 'my test',           // set name for test (e.g. if you use Sauce Labs)
+};
