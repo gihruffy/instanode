b1e4e22785a9f203b6c409024e52ef8bd464de84
Giovanni Martinelli <giovanni.artica@gmail.com>
2017-11-22 08:08:30 -0200

    logando no instagram

--- index.js
+++ index.js
@@ -6,13 +6,9 @@ const wdConfig = require('./wdio.conf');
 const userConfig = require("./user.conf");
 
 var webdriverio = require('webdriverio');
-var browser = webdriverio.remote(config);
+var browser = webdriverio.remote(wdConfig);
 
-function sleep(x) {
-    return function(cb) {
-       setTimeout(cb, x)
-    }
- }
+
 
 /*
 browser.init();
@@ -31,11 +27,25 @@ console.log(browser.getTitle());
 let driver = new Builder().forBrowser('chrome').build();
 
 driver.get('https://www.instagram.com');
-sleep(10000);
-driver.findElement(By.linkText('Faça login')).click();
+driver.sleep(2000);
+
+driver.findElement(By.linkText('Faça login')).click()
+driver.sleep(5000);
 driver.findElement(By.name("username")).sendKeys(userConfig.username);
-driver.findElement(By.name("username")).sendKeys(userConfig.password);
-driver.findElement(By.()).sendKeys(userConfig.password);
+driver.sleep(5000);
+driver.findElement(By.name("password")).sendKeys(userConfig.password);
+driver.sleep(5000);
+
+let buttonXPath = "/html[@class='js not-logged-in client-root']/body/span[@id='react-root']/section[@class='_sq4bv _29u45']/main[@class='_8fi2q _2v79o']/article[@class='_qmq8y']/div[@class='_kbq82']/div[@class='_f9sjj']/div[@class='_1zdb1']/form[@class='_3jvtb']/span[@class='_t38eb _ov9ai'][1]/button[@class='_qv64e _gexxb _4tgw8 _njrw0']";
+
+driver.findElement(By.xpath(buttonXPath)).click();
+
+driver.sleep(5000);
+
+
+
+
+
 
 
 
